99c76fde0868c4285c59929b5b19cda6f06ab3af
TonyHsu <sunprince12014@gmail.com>
2015-09-22 20:27:06 +0800

    =O

--- DE2_115/Top.sv
+++ DE2_115/Top.sv
@@ -10,16 +10,16 @@ module Top(
  *================================================================*/
 //input rst_n;
 
-parameter IDLE   = 6'2000000;
-parameter START  = 6'2000001;
-parameter FINISH = 6'2000110';
+parameter IDLE   = 6'b000000;
+parameter START  = 6'b000001;
+parameter FINISH = 6'b000110;
 
 /*================================================================*
  * REG/WIRE declarations
  *================================================================*/
 reg [6:0] cur_peak_cnt;
 reg [6:0] next_peak_cnt;
-reg [6:0] curr_state;
+reg [6:0] cur_state;
 reg [6:0] next_state;
 
 /*================================================================*
@@ -32,32 +32,34 @@ reg [6:0] next_state;
 //next state logic
 always @(*)begin
 	cur_peak_cnt = next_peak_cnt;
-	curr_state   = next_state;
-	if ((cur_peak_cnt == (2 << curr_state)) && (curr_state != FINISH ))
-		next_state = curr_state + 1;
+	cur_state   = next_state;
+	if ((cur_peak_cnt == (2 << cur_state)) && (cur_state != FINISH ))
+		next_state = cur_state + 1;
 end
 /*=============== =================================================*
  * Sequential circuit
  *================================================================*/
 always @(posedge i_clk) begin
-	if(curr_state != IDLE)
+	if(cur_state != IDLE)
 		next_peak_cnt <= cur_peak_cnt + 1;
+	else
+		next_peak_cnt <= 6'b000001;
+
 end
 
 //Means the user press the button
 always @(posedge i_start) begin
     next_state    <= START;
-	next_peak_cnt <= 6'2000000;
 end
 /*================================================================*
  * Output circuit
  *================================================================*/
 always @(*)begin
 	//send a signal to a random generator
-	if (cur_peak_cnt == (2 << curr_state) && (curr_state != FINISH))
-		o_start_rngen = 1'20;
+	if (cur_peak_cnt == (2 << cur_state) && (cur_state != FINISH))
+		o_send_rngen = 1'b0;
 	else
-		o_send_rngen = 1'21;
+		o_send_rngen = 1'b1;
 end
 	
 endmodule:Top
