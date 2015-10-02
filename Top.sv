09f336c9aed40defbc4ce6e629ba858e3a89d3e6
TonyHsu <sunprince12014@gmail.com>
2015-10-02 08:19:13 +0800

    Add counter_total bound and reorganized

--- Top.sv
+++ Top.sv
@@ -1,49 +1,51 @@
 module Top(
+	/*parameter*/
 	input i_clk,
 	input i_start,
 	input [17:0] i_sw,
 	output [6:0] o_random_out
 );
 
-	reg sw = 0;
+/*== ==============================================================*
+ * PARAMETER declarations
+ *================================================================*/
 	parameter SHOW_NUM = 15;
-	integer run_num = 0, randn = 1234;
-	reg[31:0] rdm, counter_total = 0, counter = 0, delay;
 	
+/*================================================================*
+ * REG/WIRE declarations
+ *================================================================*/
+	reg[3:0] run_num = 0;
+	reg sw = 0;
+	reg[31:0] rdm, counter_total = 0, counter = 0, delay;
+
+/*=============== =================================================*
+ * Sequential circuit
+ *================================================================*/
+
+	//Toggle
 	always @(negedge i_start) begin
 		sw <= ~sw;
 	end
 
-	always @(*) begin
-		if (sw == 1) begin
-			if (run_num == SHOW_NUM && i_sw[17])
-				o_random_out = i_sw[16:10];
-			else
-				if (i_sw == 0)
-					o_random_out = rdm % 99 + 1;
-				else
-					o_random_out = (rdm % i_sw[6:0]) + 1;
-		end
-		else begin
-			o_random_out = 0;
-		end
-	end
-	
+	//next_state logic
 	always @(posedge i_clk) begin
 		if (sw == 1) begin
 			if (run_num < SHOW_NUM) begin
 				if (counter < delay) begin
 					counter <= counter + 1;
-					counter_total <= counter_total + 1;
+					if (counter_total == '1) counter_total <= 0;
+					else counter_total <= counter_total + 1;
+
 				end
 				else begin
-					//rdm <= {rdm[4]^rdm[3],rdm[5]+1,rdm[2]+1,rdm[2:0]^rdm[2]+1,rdm[0],rdm[3]} + run_num;
 					rdm <= (22695477 * counter_total + 1) % '1;
 					run_num <= run_num + 1;
 					counter <= 0;
 				end
 			end
-
+			//run_num will reset when the next pressing
+			
+			//assign delay
 			if (run_num<5) begin
 				delay <= 37000000 - 8 * 9 * 9 * 50000;
 			end
@@ -63,6 +65,24 @@ module Top(
 		else begin
 			run_num <= 0;
 			counter <= 0;
+		end
+	end
+
+/*================================================================*
+ * Output circuit
+ *================================================================*/
+	always @(*) begin
+		if (sw == 1) begin
+			if (run_num == SHOW_NUM && i_sw[17])
+				o_random_out = i_sw[16:10];
+			else
+				if (i_sw == 0)
+					o_random_out = rdm % 99 + 1;
+				else
+					o_random_out = (rdm % i_sw[6:0]) + 1;
+		end
+		else begin
+			o_random_out = 0;
 		end
 	end
 endmodule
